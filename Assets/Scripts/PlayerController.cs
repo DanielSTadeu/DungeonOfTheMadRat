971de0f83f15b4fef67effcfdfd91f108f161cf9
DanielSTadeu <danielsaconato@hotmail.com>
2024-09-04 15:23:58 -0300

    Basic Animations
    Added a new Sprite for the player
    Added Basic idle and moving animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,7 @@ public class PlayerController : MonoBehaviour
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
     // Update is called once per frame
     void Update()
     {
@@ -22,13 +24,24 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        if (horizontal != 0)
+        {
+            animator.SetFloat("Speed", Math.Abs(horizontal));
+        }
+        else if (vertical != 0)
+        {
+            animator.SetFloat("Speed", Math.Abs(vertical));
+        }else
+        {
+            animator.SetFloat("Speed", 0f);
+        }
     }
 
     private void Flip()
     {
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-            isFacingRight= !isFacingRight;
+            isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
